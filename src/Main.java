5bf19354f8731d50c1b57b66e3338279b0abaf6b
Bkarybaev <baielkarybaev@gmail.com>
2024-11-19 20:14:25 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,97 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.LocalDate;
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Person[] persons = new Person[0];
+   static int size = 0;
+
+    public static void main(String[] args) {
+        Person person1 = new Person(1L,"aibek", "baiel@gmail.com", LocalDate.of(2004, 12, 30), "+996700047017");
+        Person person2 = new Person(2L,"Baiel", "baiel@gmail.com", LocalDate.of(2004, 12, 30), "+996700047017");
+        Person person3 = new Person(3L,"kanchoro", "baiel@gmail.com", LocalDate.of(2004, 12, 30), "+996700047017");
+        createCustomer(person1);
+        createCustomer(person2);
+        createCustomer(person3);
+
+
+        //get All
+//        System.out.println(Arrays.toString(getAllCustomers()));
+
+        //get by id
+//        getByIdCustomer(3L);
+
+        //update
+//        updateCustomer(1L,new Person(1L,"kanbolot","kanbolot@gmail.com",LocalDate.of(2004, 12, 30),"+996700047024"));
+//        getByIdCustomer(1L);
+
+        //deleted
+//        deleteCustomer(1L);
+//        deleteCustomer(2L);
+//        deleteCustomer(3L);
+//        System.out.println(Arrays.toString(getAllCustomers()));
+
+    }
+
+    static void createCustomer(Person person) {
+
+        persons = Arrays.copyOf(persons, persons.length + 1);
+        persons[persons.length - 1] = person;
+        size++;
+    }
+
+
+    public static Person[] getAllCustomers() {
+        return persons;
+    }
+
+    public static void getByIdCustomer(Long id){
+        boolean isblock = false;
+        for (Person person : persons) {
+            if (person.id.equals(id)){
+                isblock= true;
+                System.out.println("\nName :" + person.firstName);
+                System.out.println("Date of Birth :" + person.dateOfBirth);
+                System.out.println("Email :" + person.email);
+                System.out.println("Phone Number :" + person.phoneNumber);
+            }
+        }
+        if (!isblock){
+            System.out.println("not found " + id + "!!!");
+        }
+    }
+
+    public static void updateCustomer(Long id , Person person){
+        boolean isblock = false;
+        for (Person person1 : persons) {
+            if (person1.id.equals(id)){
+                isblock= true;
+                person1.setEmail(person.email);
+                person1.setId(person.id);
+                person1.setDateOfBirth(person.dateOfBirth);
+                person1.setPhoneNumber(person.phoneNumber);
+                person1.setFirstName(person.firstName);
+            }
+        }
+        if (!isblock){
+            System.out.println("not found " + id + "!!!");
+        }
+    }
+
+    public static void deleteCustomer(Long id){
+        boolean isblock = false;
+        for (int i = 0; i < size; i++) {
+            if (persons[i].id.equals(id)){
+                isblock= true;
+                for (int j = i; j < size-1; j++) {
+                    persons[j] = persons[j+1];
+                    System.out.println("deleted!!");
+                }
+                size--;
+            }
+        }
+        persons = Arrays.copyOf(persons,persons.length-1);
+        if (!isblock){
+            System.out.println("not found " + id + "!!!");
         }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Person {
+    Long id;
+    String firstName;
+    String email;
+    LocalDate dateOfBirth;
+    String phoneNumber;
+
+    public Person(Long id,String firstName, String email, LocalDate dateOfBirth, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
